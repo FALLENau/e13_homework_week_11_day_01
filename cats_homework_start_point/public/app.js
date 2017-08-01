37a9251fce905e404be8fd89fa05fc1e67b32368
FALLENau <reece.c.jones@gmail.com>
2017-08-01 08:33:38 +0100

    working pic

--- cats_homework_start_point/public/app.js
+++ cats_homework_start_point/public/app.js
@@ -5,9 +5,7 @@ var addCat = function(name, favFoodQuote, img) {
   var liPhoto = createPhoto(img);
 
   appendElements(ulCat, liName, liFood, liPhoto);
-}//name = author, favFoodQuote = quoteText
-//liName = cite, liFood = blockquote
-//ulCat = quoteArticle
+}
 
 var createUlCat = function() {
   var ul = document.createElement("ul")
@@ -22,8 +20,8 @@ var createLi = function(li) {
 }
 
 var createPhoto = function(newImg) {
-  var img = document.createElement("li")
-  name.innerText =  width="500" + newImg
+  var img = document.createElement("img")
+  img.src = newImg
   return img
 }
 
@@ -31,9 +29,8 @@ var appendElements = function(ulCat, liName, liFood, liPhoto) {
   var section = document.querySelector("#cats")
   section.appendChild(ulCat)
   ulCat.appendChild(liName)
-  liName.appendChild(liFood)
-  liFood.appendChild(liPhoto)
-  // liPhoto.appendChild()
+  ulCat.appendChild(liFood)
+  ulCat.appendChild(liPhoto)
 }
 
 var app = function() {
